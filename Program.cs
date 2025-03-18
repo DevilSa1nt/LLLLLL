da47b1a1697c68d40bfe22923e9d740b582a6afd
Artem2 <hanter.veniaminov@gmail.com>
2025-03-19 02:42:35 +0300

    edit Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
+            calc c = new calc();
 
+            List<Tuple<string, string>> t = new();
+
+            t.Add(new("капот", "синий"));
+
+            double s = c.Calc(t);
+
+            Console.WriteLine(s);
         }
     }
 }
